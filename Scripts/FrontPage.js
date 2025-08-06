e17552f5ee3996a2edb5612f668ab724b184e34d
amanagarwal0602 <amanagarwal0602@gmail.com>
2025-08-06 18:42:04 +0530

    Added Suhani's First Page View, Added Vivek's Signup Form

--- Scripts/FrontPage.js
+++ Scripts/FrontPage.js
@@ -47,3 +47,35 @@ function checkLoginState() {
     }
   });
 }
+
+function openModal() {
+    document.getElementById("loginModal").style.display = "flex";
+  }
+
+  function showSignupForm(event) {
+    event.preventDefault();
+    document.getElementById("signupForm").style.display = "flex";
+  }
+
+  function showLoginPopup(event) {
+    event.preventDefault();
+    document.getElementById("loginPopup").style.display = "flex";
+  }
+
+  function closeLoginPopup() {
+    document.getElementById("loginPopup").style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    let modal = document.getElementById("loginModal");
+    let loginPopup = document.getElementById("loginPopup");
+    
+    if (event.target === modal) {
+      modal.style.display = "none";
+      document.getElementById("signupForm").style.display = "none";
+    }
+
+    if (event.target === loginPopup) {
+      loginPopup.style.display = "none";
+    }
+  };
